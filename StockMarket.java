19bcde03e0483ff2ae4065dc8a87a5ceaab89e14
M Rashmi <150215879+Rashmimathad@users.noreply.github.com>
2025-07-02 01:52:34 +0530

    Update StockMarket.java

--- StockMarket.java
+++ StockMarket.java
@@ -7,7 +7,7 @@ class StockMarket{
 	static String castrol="Castrol India";
 	static String britannia="Britannia Inds.";
 	static String hyundai="Hyundai Motor I";
-	static String verona="GE Vernova T&D";
+	static String vernova="GE Vernova T&D";
 	static String nippon="Nippon Life Ind.";*/
 	public static void main(String stock[]){
 			System.out.println("main started");
@@ -19,10 +19,10 @@ class StockMarket{
 			String castrol="Castrol India";
 			String britannia="Britannia Inds.";
 			String hyundai="Hyundai Motor I";
-			String verona="GE Vernova T&D";
+			String vernova="GE Vernova T&D";
 			String nippon="Nippon Life Ind.";
 			
-		String stockNames[]={"Reliance Industries","P & G Hygiene","Colgate-Palmoliv","Glaxosmi Pharma","Gillette India","Castrol India","Britannia Inds.","Hyundai Motor I","GE Vernova T&D","Nippon Life Ind."};
+		String stockNames[]={relianceIndustries,hygiene,colgate,glasmoxi,gillette,castrol,britannia,hyundai,vernova,nippon};
 		System.out.println("The stock names of the stock market are : ");
 		//System.out.println(stockNames[0]+","+stockNames[1]+","+stockNames[2]+","+stockNames[3]+","+stockNames[4]+","+stockNames[5]+","+stockNames[6]+","+stockNames[7]+","+stockNames[8]+","+stockNames[9]);
 		for(String stockName : stockNames){
