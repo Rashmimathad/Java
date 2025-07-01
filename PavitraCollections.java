cfb050eb811b68ede06aee7b5d924183a4b6a210
M Rashmi <150215879+Rashmimathad@users.noreply.github.com>
2025-07-02 01:49:00 +0530

    Update PavitraCollections.java

--- PavitraCollections.java
+++ PavitraCollections.java
@@ -22,7 +22,7 @@ class PavitraCollections{
 			String zudio="Zudio";
 			String max="Max";
 			
-		String brands[]={"adidas","Louis Vuitton","Gucci","Calvin Klein","Franklin & Marshall","Tommy Jeans","U.S. POLO ASSN.","Puma","Zudio","Max"};
+		String brands[]={adidas,louisVuitton,gucci,calvinKelvin,franklin,tommy,usPolo,puma,zudio,max};
 		System.out.println("The brands of the pavithra collections are : ");
 		//System.out.println(brands[0]+","+brands[1]+","+brands[2]+","+brands[3]+","+brands[4]+","+brands[5]+","+brands[6]+","+brands[7]+","+brands[8]+","+brands[9]);
 		for(String brand : brands){
