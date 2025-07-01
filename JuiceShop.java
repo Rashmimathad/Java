d433bc73f5903b74a070ad24c8e7803ed4d00484
M Rashmi <150215879+Rashmimathad@users.noreply.github.com>
2025-07-02 01:46:59 +0530

    Update JuiceShop.java

--- JuiceShop.java
+++ JuiceShop.java
@@ -32,7 +32,7 @@ class JuiceShop{
 			String pomeGranate="Pomegranate";
 			String pineApple="Pineapple";
 			
-		String  juices[]={"Aam Panna","Mango","Orange","Watermelon","MuskMelon","Apple","Avocado","Cranberry","Dragon fruit","Grape","Guava","Lime","Lychee","Pomegranate","Pineapple"};
+		String  juices[]={aamPanna,mango,orange,waterMelon,muskMelon,apple,avocado,cranberry,dragonFruit,grape,guava,lime,lychee,pomeGranate,pineApple};
 		System.out.println("The juices of the juice shop are : ");
 		//System.out.println(juices[0]+","+juices[1]+","+juices[2]+","+juices[3]+","+juices[4]+","+juices[5]+","+juices[6]+","+juices[7]+","+juices[8]+","+juices[9]+","+juices[10]+","+juices[11]+","+juices[12]+","+juices[13]+","+juices[14]);
 		for(String juiceName : juices){
