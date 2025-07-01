4633cbfb430fe5045a96a1d4f6920c255f59c1ee
M Rashmi <150215879+Rashmimathad@users.noreply.github.com>
2025-07-02 01:50:14 +0530

    Update PeriodicTable.java

--- PeriodicTable.java
+++ PeriodicTable.java
@@ -24,7 +24,7 @@ class PeriodicTable{
 			String sodium="Sodium";
 			String aluminium="Aluminum";
 			
-		String elements[]={"Oxygen","Nitrogen","Hydrogen","Calcium","Magnesium","Helium","Sulphur","Iron","Potassium","Carbon","Sodium","Aluminum"};
+		String elements[]={oxygen,nitrogen,hydrogen,calcium,magnesium,helium,sulphur,iron,potassium,carbon,sodium,aluminum};
 		System.out.println("The elements in the periodic table are : ");
 		//System.out.println(elements[0]+","+elements[1]+","+elements[2]+","+elements[3]+","+elements[4]+","+elements[5]+","+elements[6]+","+elements[7]+","+elements[8]+","+elements[9]+","+elements[10]+","+elements[11]);
 		for(String element : elements){
